c7c832d1823265552e21da707d845eb81fc9827c
Komenpurity <jepkorirpurity8@gmail.com>
2023-03-29 12:32:23 +0300

    added a fetch request inside the submitData function

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Add your code here
+   
+
+
+    // submit data function
+    function submitData(name,email){
+
+        return fetch("http://localhost:3000/users",{
+            method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+        },
+        body: JSON.stringify({
+            name: "Steve",
+            email: "steve@steve.com",
+        }),
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log(data) 
+
+        })
+        .catch(err => console.log(err.message))  
+    }
+
+    
